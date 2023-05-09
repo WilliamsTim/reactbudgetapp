f7fd62419f5f774f70427df1d1b110f49a24cedb
timothy <williamstim146@gmail.com>
2023-05-09 08:21:34 -0500

    Add some stylings and small ui components that make a big difference in user experience.

--- src/pages/AddExpense.js
+++ src/pages/AddExpense.js
@@ -7,21 +7,28 @@ const dayjs = require("dayjs");
 function AddExpense() {
   // variables
   const [date, setDate] = useState(dayjs());
+  const [bool, setBool] = useState(true);
   const ref = useRef(null);
 
   //functions
   function validateFields() {
     // use the ref to access form fields except for date which had to be specially set.
+    // I'm thinking go by one by one validating fields and if you hit a field that isn't right, toggle the error on the associated input
+    console.log(date.$d)
+    console.log(ref.current[7].value);
+    // price input is at ref.current[3].value
+    // type input is at ref.current[5].value
 
+    return false;
   }
 
-  function handleSubmit() {
+  function handleSubmit(e) {
+    e.preventDefault();
     // submit the validated fields to the backend, remember to user withCredentials: true in axios or possibly migrate to fetch instead becasue you can just use the credentials: "include"
     if (validateFields()) {
       // submit
-    } else {
-      // don't submit, throw an error and highlight the necessary fields (possibly create a function to do that or set that functionality within the validation function)
     }
+    // if it isn't true do nothing, the inputs are not correct.
   }
 
   // component
@@ -30,12 +37,12 @@ function AddExpense() {
       <legend style={{marginTop: "15px"}}>Add Expense</legend>
       <form ref={ref} onSubmit={handleSubmit} style={{display: "flex", margin: "auto", flexDirection: "column", height: "100%", justifyContent: "space-evenly"}}>
           <DatePicker value={date} onChange={(newVal) => setDate(newVal)}/>
-          <TextField label="Price" variant="outlined" type="number" InputLabelProps={{shrink: true}}/>
+          <TextField label="Price" defaultValue={"0"} variant="outlined" type="number" InputLabelProps={{shrink: true}}/>
           <TextField label="Type" variant="outlined" InputLabelProps={{shrink: true}}/>
           <Stack direction="row" spacing={1} alignItems="center">
-            <Typography>Unnecessary</Typography>
-              <Switch defaultChecked />
-            <Typography>Necessary</Typography>
+            <Typography style={{opacity: bool ? "0.5" : "1", transition: "opacity", transitionDuration: "0.5s"}}>Unnecessary</Typography>
+              <Switch defaultChecked value={bool} onChange={() => setBool(!bool)}/>
+            <Typography style={{opacity: bool ? "1" : "0.5", transition: "opacity", transitionDuration: "0.5s"}}>Necessary</Typography>
           </Stack>
           <Button variant="outlined" type="submit" style={{width: "fit-content", marginLeft: "auto", marginRight: "auto"}}>Add Expense</Button>
       </form>
