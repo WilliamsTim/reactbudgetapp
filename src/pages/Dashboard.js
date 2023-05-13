c4ae5f0f29a321018a5521a9000b6e3d414acb8d
timothy <williamstim146@gmail.com>
2023-05-12 22:04:38 -0500

    Begin building out dashboard functionality.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Dashboard() {
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
+    const [data, setData] = useState([]);
+    var refDate = new Date();
+    const [startDate, setStartDate] = useState(new Date(refDate.getFullYear(), refDate.getMonth(), 1));
+    const [endDate, setEndDate] = useState(new Date(refDate.getFullYear(), refDate.getMonth() + 1, 0));
+    useEffect(() => {
+        fetch("http://localhost:8888/.netlify/functions/budgetAppGetExpenses?" + new URLSearchParams({startDate, endDate}), {
+            method: "GET",
+            mode: "cors",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+        .then((response) => setData(response.data));
+    }, []);
+    return (
+        <div>
+            Dashboard
+        </div>
+    )
 }
 
 export default Dashboard
