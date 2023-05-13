6cd7bd4101b4b38949eeac36dca277e8dee62d53
timothy <williamstim146@gmail.com>
2023-05-12 22:16:39 -0500

    import dayjs into the dashboard and set up the start and end dates querying the backend for the current month's expenses.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+const dayjs = require("dayjs");
 
 function Dashboard() {
     const [data, setData] = useState([]);
     var refDate = new Date();
-    const [startDate, setStartDate] = useState(new Date(refDate.getFullYear(), refDate.getMonth(), 1));
-    const [endDate, setEndDate] = useState(new Date(refDate.getFullYear(), refDate.getMonth() + 1, 0));
+    const [startDate, setStartDate] = useState(dayjs().set("year", refDate.getFullYear()).set("month", refDate.getMonth()).set("date", 0));
+    const [endDate, setEndDate] = useState(dayjs().set("year", refDate.getFullYear()).set("month", refDate.getMonth() + 1).set("date", -1));
     useEffect(() => {
-        fetch("http://localhost:8888/.netlify/functions/budgetAppGetExpenses?" + new URLSearchParams({startDate, endDate}), {
+        fetch("http://localhost:8888/.netlify/functions/budgetAppGetExpenses?" + new URLSearchParams({startDate: startDate.toISOString().slice(0, 10), endDate: endDate.toISOString().slice(0, 10)}), {
             method: "GET",
             mode: "cors",
             credentials: "include",
